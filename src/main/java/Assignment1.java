56147b01a0599c9c5966b5c648d64f41c95f7e55
Tanzim Ahmed <mr.tanzim.ahmed@gmail.com>
2025-06-01 21:32:18 +0600

    1. Create a Gradle Project for Selenium 2. Add Selenium & Testng dependency 3.  add class one code to GitHub [Selector/Locator, Launch browser etc] 3. push Selenium project to GitHub 4. Assignment me as collaborator 5. Submit Project URL as assignment

---
+++ src/main/java/Assignment1.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+import java.time.Duration;
+
+public class Assignment1 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        System.out.println("Title: "+driver.getTitle());
+
+        String projectHomeDirectory = System.getProperty("user.dir");
+        System.out.println("Project Home Directory: "+projectHomeDirectory);
+
+        driver.findElement(By.cssSelector("#user-name")).clear();
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("#password")).clear();
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+}
